f866a18c88a0bcfd61e512ac998b905e014f0b06
atenasadat <atenasaghi@yahoo.com>
2022-04-19 10:36:02 +0430

    init

--- src/main.py
+++ src/main.py
@@ -5,8 +5,8 @@ from mlassistant.main import run_main
 from mlassistant.entrypoint import BaseEntryPoint
 
 
-r''' CHANGE `project_name` to your project root package'''
-EntryPoint: Type[BaseEntryPoint] = import_module(f'project_name.entrypoints.{sys.argv[1]}').EntryPoint
+r''' CHANGE `xray_segmentation` to your project root package'''
+EntryPoint: Type[BaseEntryPoint] = import_module(f'xray_segmentation.entrypoints.{sys.argv[1]}').EntryPoint
 del sys.argv[1]
 ep = EntryPoint()
 run_main(ep.conf, ep.model, ep.parser)
